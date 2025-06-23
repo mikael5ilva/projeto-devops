12d8b1ed1ffe77d52c6a8062bfabac9e7e6ae1c7
maico <mikaelacc0unt11@gmail.com>
2025-06-23 12:48:59 -0300

    iniciando projeto node.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Me ajuda pelo amor de Deus, professor! Torço pelo senhor na maratona de São silvestre!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
